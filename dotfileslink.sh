eaacaff87f6e6b126ad92e1a757c9cfd05082f7c
Mizukoshi Keita <1313128s@stu.kobe-u.ac.jp>
2015-11-28 21:08:19 +0900

    add README file by markdown

---
+++ dotfileslink.sh
@@ -0,0 +1,7 @@
+: 1448625618:0;ln -s dotfiles/.zsh_history .
+: 1448625644:0;ln -s dotfiles/.profile .
+: 1448625650:0;ln -s dotfiles/.profile.pysave .
+: 1448628646:0;ln -s ./dotfiles/.root_hist .
+: 1448628657:0;ln -s ./dotfiles/.rootlogin.C .
+: 1448628666:0;ln -s ./dotfiles/.python_history .
+: 1448628736:0;ln -s ./dotfiles/.lesshst .
